089b06345b62b57e3e42fdd8f61af451f3d2115a
silabs-LudvigS <llsundoe@silabs.com>
2021-08-12 21:47:26 +0200

    Add SensiML data gathering over BLE in SensiML open-gateway (#40)
    Adds SensiML IMU data capture with BLE example

---
+++ platform_SensiML/platform_SensiML_DataCaptureLab/SensiML_IMU_BLE/README.md
@@ -0,0 +1,40 @@
+# SensiML IMU Data Capture IMU with BLE Example #
+
+## Summary ##
+
+This project uses the Thunderboard Sense 2 (EFR32MG12) and onboard IMU to measure accelerometer and gyroscope data and transmit the measurement data via Bluetooth Low Energy (BLE) for consumption by [SensiML's open-gateway](https://github.com/sensiml/open-gateway/). The example project use Bluetooth services along with IMU component drivers running in a bare-metal configuration. The sensor data output data rate is configured at 102 Hz.
+
+## Gecko SDK version ##
+
+v3.1
+
+## Hardware Required ##
+
+- One SLTB004A Thunderboard Sense 2 Development Kit
+<https://www.silabs.com/development-tools/thunderboard/thunderboard-sense-two-kit>
+- One micro USB cable
+
+## Setup ##
+
+Import the included .sls file to Studio then build and flash the project to the SLTB004A development kit.
+In Simplicity Studio select "File->Import" and navigate to the directory with the .sls project file.
+The project is built with relative paths to the STUDIO_SDK_LOC variable which was defined as
+
+C:\SiliconLabs\SimplicityStudio\v5\developer\sdks\gecko_sdk_suite\v3.1
+
+In Simplicity Studio, under the Debug Adapters window, right-click on the Thunderboard Sense 2 device and select "Launch console..." from the drop-down menu. In the Adapter Console window, select the "Admin" tab and type "serial vcom config speed 921600" into the terminal input. This will modify the VCOM baudrate to match the application settings. If making any changes to the USART baudrate, the baudrate change must also be modified in the VCOM debug adapter settings.
+
+## How the Project Works ##
+
+The application uses the process-action bare-metal project configuration model. The application advertises over BLE every 100 ms. The app sets the config GATT table entry statically in the GATT configuration file ({"sample_rate":102,"samples_per_packet":1,"column_location":{"AccelerometerX":0,"AccelerometerY":1,"AccelerometerZ":2,"GyroscopeX":3,"GyroscopeY":4,"GyroscopeZ":5}}). After connecting it the app can recieve notifications to transmit "data", which in this case is a single sample of acceleration and orientation data in a 6 dimensional vector.
+
+
+ 
+
+## .sls Projects Used ##
+
+SensiML_IMU_data_capture_ble.sls
+
+## How to Port to Another Part ##
+
+Open the "Project Properties" and navigate to the "C/C++ Build -> Board/Part/SDK" item.  Select the new board or part to target and "Apply" the changes.  Note: there may be dependencies that need to be resolved when changing the target architecture.
---
+++ platform_SensiML/platform_SensiML_DataCaptureLab/SensiML_IMU_BLE/src/gatt_service_sensiml.c
@@ -0,0 +1,170 @@
+/***************************************************************************//**
+ * @file
+ * @brief Top level application functions
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+*
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided \'as-is\', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ *******************************************************************************
+ * # Experimental Quality
+ * This code has not been formally tested and is provided as-is. It is not
+ * suitable for production environments. In addition, this code will not be
+ * maintained and there may be no bug maintenance planned for these resources.
+ * Silicon Labs may update projects from time to time.
+ ******************************************************************************/
+#include "em_common.h"
+#include "sl_status.h"
+#include "gatt_db.h"
+#include "app_assert.h"
+#include "app_log.h"
+#include "gatt_service_sensiml.h"
+#include "sl_imu.h"
+
+
+
+
+// -----------------------------------------------------------------------------
+// Private macros
+
+#define IMU_CP_RESPONSE                     0x10
+#define IMU_CP_OPCODE_CALIBRATE             0x01
+#define IMU_CP_OPCODE_ORIRESET              0x02
+#define IMU_CP_OPCODE_CALRESET              0x64
+#define IMU_CP_RESP_SUCCESS                 0x01
+#define IMU_CP_RESP_ERROR                   0x02
+// Client Characteristic Configuration descriptor improperly configured
+#define IMU_CP_ERR_CCCD_CONF                0x81
+
+static uint8_t imu_connection = 0;
+static bool imu_state = false; /* disabled / enabled */
+static volatile bool imu_data_notification = false;
+static int16_t data[6] = { 0, 0, 0, 0, 0, 0};
+
+
+static enum {
+  IMU_CP_IND_IDLE,
+  IMU_CP_IND_PENDING,
+  IMU_CP_IND_WAITING
+} imu_cp_indication_status = IMU_CP_IND_IDLE;
+
+// -----------------------------------------------------------------------------
+// Private function declarations
+
+static void imu_update_state(void);
+static void imu_data_notify(void);
+static void imu_connection_closed_cb(sl_bt_evt_connection_closed_t *data);
+static void imu_char_config_changed_cb(sl_bt_evt_gatt_server_characteristic_status_t *data);
+
+// -----------------------------------------------------------------------------
+// Private function definitions
+static void imu_update_state(void)
+{
+  bool imu_state_old = imu_state;
+  imu_state = imu_data_notification;
+  if (imu_state_old != imu_state) {
+      gatt_service_sensiml_imu_enable(imu_state);
+  }
+}
+
+static void imu_data_notify(void)
+{
+  sl_status_t sc;
+  sc = sl_bt_gatt_server_send_notification(
+    imu_connection,
+    gattdb_sensiml_data,
+    sizeof(data),
+    (uint8_t*)data);
+  if (sc != SL_STATUS_OK) {
+    app_log_error("[E: 0x%04x] Failed to send characteristic notification\n", (int)sc);
+  }
+}
+
+static void imu_connection_closed_cb(sl_bt_evt_connection_closed_t *data)
+{
+  (void)data;
+  imu_data_notification = false;
+  imu_cp_indication_status = IMU_CP_IND_IDLE;
+  imu_update_state();
+}
+
+static void imu_char_config_changed_cb(sl_bt_evt_gatt_server_characteristic_status_t *data)
+{
+  bool enable = gatt_disable != data->client_config_flags;
+  imu_connection = data->connection;
+  // update notification status
+  switch (data->characteristic) {
+    case gattdb_sensiml_data:
+      imu_data_notification = enable;
+      break;
+    default:
+      app_assert(false, "Unexpected characteristic\n");
+      break;
+  }
+  imu_update_state();
+}
+
+
+void gatt_service_sensiml_imu_on_event(sl_bt_msg_t *evt){// Handle stack events
+  switch (SL_BT_MSG_ID(evt->header)) {
+    case sl_bt_evt_connection_closed_id:
+      imu_connection_closed_cb(&evt->data.evt_connection_closed);
+      break;
+    case sl_bt_evt_gatt_server_characteristic_status_id:
+      if ((gatt_server_client_config == (gatt_server_characteristic_status_flag_t)evt->data.evt_gatt_server_characteristic_status.status_flags)
+          && ((gattdb_sensiml_data == evt->data.evt_gatt_server_user_read_request.characteristic))) {
+        // client characteristic configuration changed by remote GATT client
+        imu_char_config_changed_cb(&evt->data.evt_gatt_server_characteristic_status);
+      }
+      break;
+  }
+}
+
+void gatt_service_sensiml_imu_step(void){
+  if (imu_state) {
+      if (SL_STATUS_OK == gatt_service_sensiml_imu_get(data)) {
+        if (imu_data_notification) {
+          imu_data_notify();
+        }
+      }
+    }
+}
+
+sl_status_t gatt_service_sensiml_imu_get(int16_t data[6]){
+  sl_imu_update();
+  while (!sl_imu_is_data_ready()) {
+      // wait
+  }
+  sl_imu_get_acceleration(data);
+  sl_imu_get_orientation(&data[3]);
+  return SL_STATUS_OK;
+}
+
+void gatt_service_sensiml_imu_enable(bool enable)
+{
+  sl_imu_init();
+  sl_imu_configure(102);
+  (void)enable;
+}
+
+
