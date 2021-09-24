14e24d86c684d784409e79cb81e4c2bbfc1c2f36
Sarah Latiolais <salatiol@silabs.com>
2021-09-24 15:03:52 -0500

    Adds platform_lcd_si70xx example for EFR32FG23 and EFM32PG23 for SDK v3.2

---
+++ platform_lcd_si70xx/README.md
@@ -0,0 +1,54 @@
+# Segment LCD and Temperature Sensor Example
+
+## Summary
+This project demonstrates how to use the Si70xx temperature sensor to measure
+and record temperature values and how to use the Segment LCD to display those
+values. 
+
+## Gecko SDK Version
+v3.2
+
+## Hardware Required
+Listed below are the port and pin mappings for working with this example.
+
+* Board:  Silicon Labs EFR32FG23 Dev Kit Board (BRD2600A) 
+	* Device: EFR32FG23B010F512IM48
+* Board:  Silicon Labs EFM32PG23 Pro Kit Board (BRD2504A) 
+	* Device: EFM32PG23B310F512IM48
+
+## Setup
+1. Clone this repository from GitHub onto your local machine.
+2. Open Simplicity Studio IDE and navigate to Project > Import > MCU project.
+3. Click the browse button and navigate to the local repository folder.
+4. Select the .sls file for the board, click Next twice, and then click Finish.
+5. Build the project and download it to the Board.
+6. Reset the board by pressing the Reset button on the board.
+7. Observe the LCD displaying 00000 after the board is reset.
+8. After 5 seconds, the LCD will update with the current temperature reading 
+from the Si70xx RHT sensor. Every 5 seconds the LCD will update with the new
+temperature reading.
+
+## How the Project Works
+The project uses a periodic sleeptimer that executes a callback function every
+5,000ms. This callback function calls APIs from the Si70xx driver library that
+measure and read the temperature and relative humidity values from the sensor. 
+This value is then displayed on the segment LCD using APIs from the Segment LCD
+driver library. 
+
+## .sls Projects Used
+* platform_lcd_si70xx_fg23.sls
+* platform_lcd_si70xx_pg23.sls
+
+## How to Port to Another Part
+Right click on the project and select "Properties" and navigate to "C/C++ 
+Build" then "Board/Part/SDK". Select the new board or part to target and apply 
+the changes. There may be some dependencies that need to be resolved when 
+changing the target architecture. 
+
+## Note
+Although measures have been taken to thermally isolate the sensor from the 
+board, temperature readings will be influenced when power is dissipated on the
+board. More accurate temperature measurements are achieved when powering the 
+board with a battery or through the Mini Simplicity connector as self-heating 
+from the on-board LDO is eliminated and the on-board debugger is put in a low
+power state.
---
+++ platform_lcd_si70xx/src/app.c
@@ -0,0 +1,128 @@
+/***************************************************************************//**
+ * @file
+ * @brief Top level application functions
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2021 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ *******************************************************************************
+ * # Experimental Quality
+ * This code has not been formally tested and is provided as-is. It is not
+ * suitable for production environments. In addition, this code will not be
+ * maintained and there may be no bug maintenance planned for these resources.
+ * Silicon Labs may update projects from time to time.
+ ******************************************************************************/
+#include "sl_segmentlcd.h"
+#include "sl_i2cspm.h"
+#include "sl_i2cspm_instances.h"
+#include "sl_si70xx.h"
+#include "sl_sleeptimer.h"
+
+#define TIMEOUT_MS         5000    // Periodic timer duration in ms
+#define NEGATIVE_CASE      0        // Used when a negative temperature is read
+#define THREE_DIGIT_CASE   100000   // Used when a three-digit temperature is read
+
+uint32_t rh_data = 0;               // Relative humidity data
+int32_t temp_data = 0;              // Temperature data
+
+static sl_sleeptimer_timer_handle_t periodic_timer; // Handle for periodic timer
+
+/***************************************************************************//**
+ * Periodic timer callback function
+ ******************************************************************************/
+ void on_periodic_timeout(sl_sleeptimer_timer_handle_t *handle,
+                                 void *data)
+ {
+   // This prevents unused parameter warnings
+   (void)&handle;
+   (void)&data;
+
+   // Measure and read the values for Relative Humidity and Temperature
+   sl_si70xx_measure_rh_and_temp(sl_i2cspm_sensor, SI7021_ADDR, &rh_data, &temp_data);
+   sl_si70xx_read_rh_and_temp(sl_i2cspm_sensor, SI7021_ADDR, &rh_data, &temp_data);
+
+/***************************************************************************//**
+ * The following statements are written to handle the cases when the sensor
+ * measures negative temperature values, three digit temperature values, and
+ * temperature values from 0-100 degrees Celsius. Keep in mind, the Si7021 part
+ * has an operating temperature range of -40 to +125 degrees Celsius.
+ * The temp_data variable stores a temperature read by the Si7021 that is
+ * multiplied by a factor of 1000. In order to display the temperature correctly
+ * in the negative and three digit cases, the temp_data variable must be divided
+ * by 10 to remove one digit from the variable. Without this, the value of
+ * temp_data passed to the SegmentLCD_Number() function may be higher than the
+ * max value the Segment LCD could display.
+ ******************************************************************************/
+   // Used when the temperature is below 0 degrees Celsius
+   if (temp_data < NEGATIVE_CASE) {
+       temp_data = temp_data/10;
+       SegmentLCD_Number(temp_data);  // Display the value of temp_data
+       SegmentLCD_Symbol(LCD_SYMBOL_DEGC, 1); // Display Degree C symbol
+       SegmentLCD_Symbol(LCD_SYMBOL_P3, 1); // Display decimal symbol
+     }
+
+   // Used when the temperature is above 100 degrees Celsius
+   if (temp_data >= THREE_DIGIT_CASE){
+       temp_data = temp_data/10;
+       SegmentLCD_Number(temp_data); // Display the value of temp_data
+       SegmentLCD_Symbol(LCD_SYMBOL_DEGC, 1); // Display Degree C symbol
+       SegmentLCD_Symbol(LCD_SYMBOL_P3, 1); // Display decimal symbol
+   }
+
+   // Used when the temperature is between 0-100 degrees Celsius
+   else{
+       SegmentLCD_Number(temp_data);  // Display the value of temp_data
+       SegmentLCD_Symbol(LCD_SYMBOL_DEGC, 1); // Display Degree C symbol
+       SegmentLCD_Symbol(LCD_SYMBOL_P2, 1); // Display decimal symbol
+   }
+
+ }
+
+/***************************************************************************//**
+ * Initialize application.
+ ******************************************************************************/
+void app_init(void)
+{
+  // Initialize the Si7021 sensor, sleeptimer, and Segment LCD display
+  sl_si70xx_init(sl_i2cspm_sensor, SI7021_ADDR);
+  sl_sleeptimer_init();
+  SegmentLCD_Init(true);
+
+  // Display all 0's upon initialization
+  SegmentLCD_Number(0);
+}
+
+/***************************************************************************//**
+ * App ticking function.
+ ******************************************************************************/
+void app_process_action(void)
+{
+  // Periodic timer for updating the temperature value displayed
+  sl_sleeptimer_start_periodic_timer_ms(&periodic_timer,
+                                        TIMEOUT_MS,
+                                        on_periodic_timeout,
+                                        NULL,
+                                        0,
+                                        0);
+}
