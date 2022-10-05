4b831cf6ace04d69bbdb95d55e8250cb8d45ddd6
silabs-Ruibin <66031031+silabs-Ruibin@users.noreply.github.com>
2022-10-05 13:58:25 -0500

    Adds lcd animation and blink example for fg23 (#74)

---
+++ platform_lcd_animation_blink_fg23/README.md
@@ -0,0 +1,72 @@
+# Segment LCD with Animation and Blink Functionality
+
+## Summary
+This project shows how to use the FG23 LCD peripheral on the BRD2600A board to perform
+auto animation and blink without CPU intervention, and displaying a pattern
+on the segment LCD (more information on "How it works" below).
+
+## Gecko SDK version
+v4.1.1
+
+## Hardware Required
+
+* Board:  Silicon Labs EFR32FG23 Dev Kit (BRD2600A)
+	* Device: EFR32FG23B010F512GM48
+
+## Connections Required
+Connect the board via a micro-USB cable to your PC to flash the example.
+
+## Setup
+1. Clone the repository with this project from GitHub onto your local machine.
+
+2. From within the Simplicity Studio IDE, select Import -> MCU Project... from the Project menu.
+
+3. Click the Browse button and navigate to the local repository folder, then to the SimplicityStudio folder.
+
+4. Click the Next button twice and then click Finish.
+
+## How It Works
+The LCD peripheral has the capability to do animation without involving the LCD data
+registers as well as the CPU. It allows specialized patterns running on the LCD panel while the 
+microcontoller remains in low energy mode. A max of 8 segments can be used for animation feature
+and they can either be segments 0-7 controlled by COM0 or segments 8-15 controlled by COM0. The animation
+is implemented as two programmable 8 bit registers that are shifted either left or right every other
+animation state for a total of 16 states. The LCD_AREGA register is shifted every odd state and the
+LCD_AREGB register is shifted every even state. The two registers can either be OR'ed or AND'ed to achieve
+the desired animation pattern. The animation state machine is described in section 27.3.13.3 of the reference manual.
+
+The BRD2600A board only maps segment 0-1 and segment 4-7 to the segment LCD panel, therefore the animation
+feature can only control 6 physical LCD segments on this board.
+
+The LCD peripheral also has the capability to blink at a frequency given by CLKevent every 2Hz. The segments will be
+alternating between on and off when the LCD is blinking. Refer to section 27.3.13.1 of the reference manual
+for more information regarding the blinking feature.
+
+For this example, the LCD segments controlled by the animation feature are: 1D, 1C, 2D, P2, 3D, P3. 
+Refer to the BRD2600A schematic for additional information.
+
+![fg23_lcd_segment_mapping](https://user-images.githubusercontent.com/66031031/193937564-7755558f-d8a2-4e97-b436-760965fd6afa.png)
+	
+To test:
+1. Build and flash the hex image onto the board. Reset board and observe the
+   segment LCD displaying animation at a 2Hz rate.
+2. Change BLINK_ENABLE define on line 30 of app.c file to 1
+3. Rebuild and flash the hex image onto the board. Reset board and observe the
+   segment LCD displaying animation and blinking at a 2Hz rate. 
+   This example runs as it is and requires no user intervention. 
+
+https://user-images.githubusercontent.com/66031031/193944633-d7a458a9-49b9-4eaa-afec-87ecd30036e5.mov
+
+**Note**:
+As seen in the video above, the LCD will be blank for 2 seconds since segments 2 and 3 are not
+mapped to the onboard LCD. Blink feature is not enabled in the video. 
+
+## .sls Projects Used
+* platform_lcd_animation_blink_fg23.sls
+
+## How to Port to Another Part
+Right click on the project and select "Properties" and navigate to "C/C++ 
+Build" then "Board/Part/SDK". Select the new board or part to target and apply 
+the changes. There may be some dependencies that need to be resolved when 
+changing the target architecture. This example can only run out of the box on
+BRD2600A device.
---
+++ platform_lcd_animation_blink_fg23/src/app.c
@@ -0,0 +1,140 @@
+/***************************************************************************//**
+ * @file
+ * @brief Top level application functions
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+
+#include "em_device.h"
+#include "em_chip.h"
+#include "em_cmu.h"
+#include "em_emu.h"
+#include "em_gpio.h"
+#include "em_lcd.h"
+
+#define LCD_PRESC           16
+#define FRAME_RATE_DIV      8
+#define VLCD                3.0
+#define FRAME_COUNTER_TOP   16
+
+#define BLINK_ENABLE        0
+
+/***************************************************************************//**
+ * @brief  Initializes the LCD to enable 1 COM x 6 SEG, set the framerate to
+ *         32Hz, and set the frame counter event to trigger at 2Hz.
+ ******************************************************************************/
+void initLCD(void)
+{
+  // Enable clocks required for LCD
+  CMU_ClockEnable(cmuClock_LFRCO, true);
+  CMU_ClockEnable(cmuClock_LCD, true);
+  CMU_ClockEnable(cmuClock_GPIO, true);
+
+  // Initialize the LCD
+  LCD_Init_TypeDef lcd_init = {
+    // Don't enable LCD driver after init to continue configuring the LCD
+    .enable = false,
+
+    // The LCD has 4 COMs and is driven with 1/3 bias (4 voltage levels)
+    .mux = lcdMuxQuadruplex,
+    .bias = lcdBiasOneThird,
+
+    // In order to minimize power consumption, the low power waveform is used
+    .wave = lcdWaveLowPower,
+
+    // Step down mode selected because VDD = 3.3V and the desired VLCD = 3.0V
+    .mode = lcdModeStepDown,
+
+    // Table 28.1 in the reference manual shows 0.0% charge redistribution with
+    // currently configured mux and prescaler values, so just keep it disabled
+    .chargeRedistribution = lcdChargeRedistributionDisable,
+
+    // Frame rate = LCD_CLK / (8 * FRAME_RATE_DIV)
+    //            =  2048Hz / (8 * 8)
+    //            =  32Hz
+    .frameRateDivider = FRAME_RATE_DIV - 1,
+
+    // Contrast level = (VLCD - 2.25V) / 0.05V
+    //                = (3.0V - 2.25V) / 0.05V
+    //                = 15
+    .contrastLevel = (int)((VLCD - 2.25) / 0.05)
+  };
+
+  LCD_Init(&lcd_init);
+
+  // LCD_CLK = LFRCOfreq / LCD_PRESC
+  //         =   32768Hz / 16
+  //         =    2048Hz
+  LCD->CTRL = (LCD->CTRL & (~_LCD_CTRL_PRESCALE_MASK))
+              | ((LCD_PRESC - 1) << _LCD_CTRL_PRESCALE_SHIFT);
+
+  // Select LCD VDDX to AVDD to enable step down mode
+  LCD->BIASCTRL_SET = LCD_BIASCTRL_VDDXSEL_AVDD;
+
+  // Enable common (COM0) and segment (SEG0-SEG1, SEG4-SEG7) lines
+  // BRD2600A has only segment 0-1 and 4-7 to configure with
+  LCD_ComEnable(0, true);
+  LCD_SegmentEnable(0, true);
+  LCD_SegmentEnable(1, true);
+  LCD_SegmentEnable(4, true);
+  LCD_SegmentEnable(5, true);
+  LCD_SegmentEnable(6, true);
+  LCD_SegmentEnable(7, true);
+
+  // Animation state 0 has segment 0 on
+  // Odd state will shift AReg to the left by 1 bit
+  // AReg initial value is 1000 0000, BReg initial value is 0000 0000
+  LCD_AnimInit_TypeDef anim_init = {
+    .enable = true,
+    .AReg = 0x80,
+    .AShift = lcdAnimShiftLeft,
+    .BReg = 0x00,
+    .BShift = lcdAnimShiftNone,
+    .animLogic = lcdAnimLogicOr,
+    .startSeg = lcdAnimLocSeg0To7
+  };
+
+  LCD_AnimInit(&anim_init);
+
+#if BLINK_ENABLE
+  // Set BLINK_ENABLE define to 1 to enable blinking. Blinking will turn off
+  // all segments every other frame counter event.
+  LCD_BlinkEnable(true);
+#endif
+
+  // Frame count event frequency = frame rate / (FRAME_COUNTER_TOP * prescale)
+  //                             = 32Hz       / (16                * 1)
+  //                             = 2Hz
+  LCD_FrameCountInit_TypeDef fc_init = {
+    .enable = true,
+    .top = FRAME_COUNTER_TOP,
+    .prescale = lcdFCPrescDiv1
+  };
+
+  LCD_FrameCountInit(&fc_init);
+}
+
+/***************************************************************************//**
+ * Initialize application.
+ ******************************************************************************/
+void app_init(void)
+{
+  initLCD();
+}
+
+/***************************************************************************//**
+ * App ticking function.
+ ******************************************************************************/
+void app_process_action(void)
+{
+}
